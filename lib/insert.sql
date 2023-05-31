eee950d4b55c80d768c389d1c4e23b464d9f5123
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-05-31 09:45:11 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+ INSERT INTO [Sex] ['M ,'F']
+( ['M', 'F']
+. [Mr. Choclate]
+.[Rowdy]
+.[Tabitha]
+.[Sergeant Brown]
+.[Melissa]
+.[Grinch]
+.[Wendy]
+.[unamed]
+
+)
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,19 +1,30 @@
 def selects_all_female_bears_return_name_and_age
   "Write your SQL query here"
+"SELECT bears.name, bears.age FROM  bears WHERE  sex='F'"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "Write your SQL query here"
+  "SELECT  bears.name, bear.order FROM bear WHERE  order='A'"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "Write your SQL query here"
+
+ "SELECT bear.name,  bear .age FROM  bear WHERE order = 'Alive and Young'"
+
 end
 
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
+
+"SELECT bear.name, bear.oldest.age FROM bear WHERE   order='Oldest'"
+
+
 end
 
 def select_youngest_bear_and_returns_name_and_age
   "Write your SQL query here"
+
+  "SELECT bear.name, bear.youngest_bear_name, bear.youngest_bear_age FROM bear WHERE    order ='Oldest'"
 end
